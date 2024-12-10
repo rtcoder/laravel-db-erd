ac11af07a738124d705dd012050a02f60d9d9432
Dawid Jeż <dawidjez@gmail.com>
2024-12-10 14:29:20 +0100

    Update ERD generation command with flexible options
    Refactor the command to support additional options like output format, file path, and database driver for better customization. Improved error handling and user feedback to enhance usability and debugging. Adjusted default configurations for streamlined setup and execution.

--- src/Commands/GenerateERDCommand.php
+++ src/Commands/GenerateERDCommand.php
@@ -1,22 +1,57 @@
 <?php
+
 namespace Rtcoder\LaravelERD\Commands;
 
-use Exception;
 use Illuminate\Console\Command;
 use Rtcoder\LaravelERD\Services\ERDGenerator;
+use Symfony\Component\Console\Command\Command as CommandAlias;
 
 class GenerateERDCommand extends Command
 {
-    protected $signature = 'erd:generate';
-    protected $description = 'Generates an ERD diagram';
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'db:erd 
+                            {--output= : The file path where the ERD will be saved (default: storage/erd/erd_diagram.<format>)} 
+                            {--driver= : The database driver to use (default: from config file)} 
+                            {--format= : The output format for the ERD (e.g., pdf, png, svg)}';
 
     /**
-     * @throws Exception
+     * The console command description.
+     *
+     * @var string
      */
-    public function handle(): void
+    protected $description = 'Generate an Entity-Relationship Diagram (ERD) of the database schema';
+
+    /**
+     * Execute the console command.
+     *
+     * @return int
+     */
+    public function handle(): int
     {
-        $generator = new ERDGenerator();
-        $path = $generator->generate();
-        $this->info("ERD diagram saved to: $path");
+        $format = $this->option('format') ?? config('erd.output_format');
+        $outputDirectory = config('erd.output_directory');
+        $outputName = config('erd.output_name');
+        $outputPath = $this->option('output') ?? "{$outputDirectory}/{$outputName}.{$format}";
+        $driver = $this->option('driver') ?? config('erd.default_driver');
+
+        // Informacje
+        $this->info("Generating ERD using driver: {$driver}");
+        $this->info("Output format: {$format}");
+        $this->info("Saving ERD to: {$outputPath}");
+
+        try {
+            $generator = new ERDGenerator();
+            $generator->generate($outputPath, $driver);
+
+            $this->info('ERD successfully generated!');
+            return CommandAlias::SUCCESS;
+        } catch (\Exception $e) {
+            $this->error('Failed to generate ERD: ' . $e->getMessage());
+            return CommandAlias::FAILURE;
+        }
     }
 }
