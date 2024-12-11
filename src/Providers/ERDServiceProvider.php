d890d561390cd4551f30ad1649924ccfe153cf19
Dawid Jeż <dawidjez@gmail.com>
2024-12-11 14:06:23 +0100

    Enhance view handling and resource organization
    Implemented fallback logic for view rendering to improve reliability when files are missing. Updated namespace and adjusted paths for better project structure and clarity. Added view publishing and singleton binding for `ERDGenerator` to streamline service registration.

--- src/Providers/ERDServiceProvider.php
+++ src/Providers/ERDServiceProvider.php
@@ -1,27 +1,48 @@
 <?php
-namespace Rtcoder\LaravelERD\Providers;
+
+namespace Rtcoder\LaravelERD;
 
 use Illuminate\Support\ServiceProvider;
 use Rtcoder\LaravelERD\Commands\GenerateERDCommand;
+use Rtcoder\LaravelERD\Services\ERDGenerator;
 
 class ERDServiceProvider extends ServiceProvider
 {
+    /**
+     * Bootstrap any application services.
+     *
+     * @return void
+     */
     public function boot(): void
     {
+        $this->loadViewsFrom(__DIR__ . '/../resources/views', 'laravel-erd');
         $this->publishes([
-            __DIR__.'/../../config/erd.php' => config_path('erd.php'),
-        ]);
+            __DIR__ . '/../resources/views' => resource_path('views/vendor/laravel-erd'),
+        ], 'views');
+
+        $this->publishes([
+            __DIR__ . '/../config/erd.php' => config_path('erd.php'),
+        ], 'config');
 
         if ($this->app->runningInConsole()) {
             $this->commands([GenerateERDCommand::class]);
         }
     }
 
+    /**
+     * Register any application services.
+     *
+     * @return void
+     */
     public function register(): void
     {
         $this->mergeConfigFrom(
-            __DIR__.'/../../config/erd.php',
+            __DIR__ . '/../config/erd.php',
             'erd'
         );
+
+        $this->app->singleton('erd-generator', function ($app) {
+            return new ERDGenerator();
+        });
     }
 }
